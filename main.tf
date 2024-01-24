578e122fa30e3126e3848a61b6e58ae68ac5a8c4
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2024-01-24 11:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
+provider "azurerm" {
+  features = {}
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "example1"
-  location = "West Europe"
+  name     = "example-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  count                = 3
+  name                 = "subnet${count.index + 1}"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.${count.index + 1}.0/24"]
 }
