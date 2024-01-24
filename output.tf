4a9f8afe913ee380e7fe204d253b0a58a448fe02
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2024-01-24 15:54:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "subnet_info" {
-  value = [
-    for idx, subnet in azurerm_subnet.example : {
+  value = {
+    for idx, subnet in azurerm_subnet.example : subnet.name => {
       name                 = subnet.name
       resource_group_name  = subnet.resource_group_name
       virtual_network_name = subnet.virtual_network_name
       address_prefixes     = subnet.address_prefixes
     }
-  ]
+  }
 }
